69c6dc230bd172d67787d77ecc9d1b50d2389b77
Mel <jones.mel@gmail.com>
2023-03-13 16:38:34 +0100

    Add the Observer Pattern * The observer pattern is used to notify a set of interested parties when a state change occurs. * The rxjs library is a popular implementation of this pattern.

---
+++ typescript-10/behavioral/observer.ts
@@ -0,0 +1,16 @@
+//  Observer | Behavioral Pattern
+// The observer pattern is used to notify a set of interested parties when a state change occurs.
+// The rxjs library is a popular implementation of this pattern.
+
+import { subject } from 'rxjs';
+
+const news = new Subject();
+
+const tv1 = news.subscribe((v) => console.log(v + 'via Den TV'));
+const tv2 = news.subscribe((v) => console.log(v + 'via Batcave TV'));
+const tv3 = news.subscribe((v) => console.log(v + 'via Airport TV'));
+
+news.next('Breaking News: ');
+news.next('The war is over');
+
+tv1.unsubscribe();
