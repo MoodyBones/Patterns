45ce0fea221a0db8b238f9b8db2eecf5a12a478c
Mel <jones.mel@gmail.com>
2023-03-13 16:33:23 +0100

    Add notes about iterator and range function written in JS

--- typescript-10/behavioral/iterator.ts
+++ typescript-10/behavioral/iterator.ts
@@ -1,3 +1,14 @@
+// Iterator | Behavioral pattern
+// https://fireship.io/lessons/typescript-design-patterns/
+//
+// Allows you to traverse through a collection of objects/elements
+// Pull based system
+
+// Range function
+// Most programming languages provide abstractions for iteration like the for loop.
+// However, you can create your own iterators in JavaScript by using the Symbol.iterator property.
+// The code below creates a custom range function that can be used in a regular for loop.
+
 function range(start: number, end: number, step = 1) {
   return {
     [Symbol.iterator]() {
